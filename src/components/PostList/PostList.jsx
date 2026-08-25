e4bf4a481f731db5102a02e4c8e62af09d9a8511
vasylyx <mr.vasilishin@gmail.com>
2026-08-25 21:07:10 +0300

    solve

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,9 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
+
+const MakePostInfo = posts =>
+  posts.map(post => <PostInfo post={post} key={post.id} />);
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">{MakePostInfo(posts)}</div>
+);
